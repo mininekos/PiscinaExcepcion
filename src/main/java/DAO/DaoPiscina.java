ea5592b7f727c2088835a2475a4621369e819cf8
mininekos <manuel2g2m@gmail.com>
2022-10-07 13:45:43 +0200

    V1.2

--- src/main/java/DAO/DaoPiscina.java
+++ src/main/java/DAO/DaoPiscina.java
@@ -4,35 +4,37 @@
  */
 package DAO;
 
+import Modelo.Piscina;
 import java.util.ArrayList;
 
 
 public class DaoPiscina {
     
-    private static ArrayList<Integer> lista;
+    private static ArrayList<Piscina> lista;
+
     private DaoPiscina(){
     
     }
-    public static ArrayList<Integer> getInstance(){
+    public static ArrayList<Piscina> getInstance(){
         if(lista==null){
-		lista=new ArrayList<Integer>();   
+		lista=new ArrayList<Piscina>();   
             }
 	return lista;
     }
-    private ArrayList<Integer> CrearPiscinas(){
-        lista= new ArrayList<Integer>();
-        lista.add(1000);
-        lista.add(1100);
-        lista.add(1200);
-        lista.add(1300);
-        lista.add(1400);
-        lista.add(1500);
-        lista.add(1600);
-        lista.add(1700);
-        lista.add(1800);
-        lista.add(1900);
-        lista.add(2000);
-        
+    private ArrayList<Piscina> CrearPiscinas(){
+        lista= new ArrayList<Piscina>();
+        lista.add(new Piscina(1000));
+        lista.add(new Piscina(1100));
+        lista.add(new Piscina(1200));
+        lista.add(new Piscina(1300));
+        lista.add(new Piscina(1400));
+        lista.add(new Piscina(1500));
+        lista.add(new Piscina(1600));
+        lista.add(new Piscina(1700));
+        lista.add(new Piscina(1800));
+        lista.add(new Piscina(1900));
+        lista.add(new Piscina(2000));
+
         return lista;
     }
 }
