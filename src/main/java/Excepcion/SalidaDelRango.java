8511a6ab22f01ea2bcfaa4cac0f75083c2370c1a
mininekos <manuel2g2m@gmail.com>
2022-10-07 10:18:45 +0200

    first commit

---
+++ src/main/java/Excepcion/SalidaDelRango.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepcion;
+
+/**
+ *
+ * @author Dam
+ */
+public class SalidaDelRango extends Exception{
+    
+    public SalidaDelRango(String cadena) {
+        super(cadena);
+    }
+    
+}
---
+++ src/main/java/Modelo/Piscina.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import Excepcion.SalidaDelRango;
+
+/**
+ *
+ * @author Dam
+ */
+public class Piscina {
+    private int nivel; 
+    public final int MAX_NIVEL;
+    
+    public Piscina(int max){
+        if (max<0) max=0;
+        MAX_NIVEL=max;
+    }
+    
+    public int getNivel(){
+        return nivel;}
+    
+    public void vaciar(int cantidad) throws SalidaDelRango{
+       
+            int temporal = nivel - cantidad;
+            if (temporal<0 || cantidad<MAX_NIVEL)
+                throw new SalidaDelRango("Error desvordamiento");
+            else{
+                nivel = temporal;
+            }
+        
+        }
+    
+     public void llenar(int cantidad) throws SalidaDelRango{
+       
+            int temporal = nivel + cantidad;
+            if (temporal>MAX_NIVEL)
+                throw new SalidaDelRango("Error no hay sufiente agua");
+            else{
+                nivel = temporal;
+            }
+        
+        }
+}
